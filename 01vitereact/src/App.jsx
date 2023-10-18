ca633a16c1b3f3669e9a8aee89a305632afb2959
jvamshikrishna <jvamshikrishna1997@gmail.com>
2023-10-17 21:19:57 -0500

    2nd commit

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,15 @@
-
+import Chai from "./chai"
 
 function App() {
 
 
   return (
-    <h1>
-      Hello Krishna
-    </h1>
+    <> {/*only 1 tag to be returned so, instead of using div tag , we can create a fragment empty tag*/}
+    <Chai/>
+    <h1>vamshi</h1>
+    </>
+      
+    
   )
 }
 
