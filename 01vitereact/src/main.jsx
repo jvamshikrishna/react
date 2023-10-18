78ac3f463d89f9a82334de05f37a03e4dba1918a
jvamshikrishna <jvamshikrishna1997@gmail.com>
2023-10-17 22:26:46 -0500

    create own react library

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>visit google</a>
+)
+
+const reactElement2 = React.createElement(
+  'a',
+  {href:'https://google.com', target: '_blank'},
+  'click me to visit google'
+
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+// reactElement // not working
+//anotherElement // working
+reactElement2 // working
 )
